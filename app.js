eb68b0c7e7e55c02c730f97f228a20eeded37d3b
Cyrillo46 <yahooisbetter46@gmail.com>
2022-07-29 22:59:43 -0400

    card displayed to browser when 'Draw' is clicked

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ let deckId = "";
 function handleClick() {
   fetch("https://www.deckofcardsapi.com/api/deck/new/")
     .then((res) => res.json())
-    .then((data) => (data.deck_id = deckId));
+    .then((data) => (deckId = data.deck_id));
+  console.log(deckId);
+}
+
+function drawCard() {
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("card-display").innerHTML = `
+            <img src="${data.cards[0].image}" />
+      `;
+    });
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
+document.getElementById("draw-card").addEventListener("click", drawCard);
