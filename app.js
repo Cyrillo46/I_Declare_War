ba55dfc111291aef7bcc73fe6c247a0a1eee464b
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-01 22:56:47 -0400

    determineWinner function added and tested

--- app.js
+++ app.js
@@ -22,5 +22,39 @@ function drawCard() {
     });
 }
 
+function determineWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+
+  let card1Index = valueOptions.indexOf(card1);
+  let card2Index = valueOptions.indexOf(card2);
+
+  if (card1Index > card2Index) {
+    console.log("player 1 wins");
+  } else if (card1Index === card2Index) {
+    console.log("It's a draw");
+  } else {
+    console.log("player 2 wins");
+  }
+}
+
 document.getElementById("new-deck").addEventListener("click", handleClick);
 document.getElementById("draw-card").addEventListener("click", drawCard);
+
+let card1 = "7";
+let card2 = "KING";
+
+determineWinner(card1, card2);
